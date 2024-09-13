feeca0934b354d8f319c75899a95c95c84bea3f5
Poliana Fantin <66546064+kittulu@users.noreply.github.com>
2024-09-12 21:54:25 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { CountryItem } from "./components/country-item";
+import { Header } from "./components/header";
+
+export default function App() {
+
+  const [list, setlista] = useState([]);
+
+  async function fetchCountries() {
+    const req = await fetch(" https://restcountries.com/v3.1/all?fields=name,flags");
+    const data = await req.json();
+    setlista(data);
+    //   console.log('resultado',data);
+  }
+
+  //executa apenas quando o componente for criado.
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <main>
+      <Header title="LISTA DE PAÍSES" />
+      <ul className="list-container">
+
+        {list.map((item) => {
+          return <CountryItem key={item.flags.png}
+            url={item.flags.png} name={item.name.common} />;
+        })
+        }
+      </ul>
+    </main>
+  );
+}
