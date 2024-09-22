9796fc590add62c0299ebc2dbe458f9f7c32592d
Poliana Fantin <66546064+kittulu@users.noreply.github.com>
2024-09-21 22:21:25 -0400

    Add files via upload (#1)

--- src/components/country-item.jsx
+++ src/components/country-item.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function CountryItem({ url, name }) {
-    return (
+  return (
+    <Link to={`/country/${name}`}>
       <li className="item-container">
         <img src={url} className="item-image" />
         <h2 className="item-name">{name}</h2>
       </li>
-    );
-  }
+    </Link>
+  );
+}
